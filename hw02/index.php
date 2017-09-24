864bb84e5cb6be55a6765f62d7be00721456d073
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2017-09-24 13:20:03 -0400

    HW02: Add bootstrap

--- hw02/index.php
+++ hw02/index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <!-- Peter Nguyen -->
     <!-- CSCI 297 -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>Assignment 2</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <style>
+        body {
+            padding: 2rem;
+        }
+    </style>
 </head>
 <body>
 <p>Hello World. The next 7 days are:</p>
